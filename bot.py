c5657568219cc04af9c7f94d54f29e4d6cd6c3fb
amuq <67078527+amuq@users.noreply.github.com>
2020-06-17 22:29:38 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+name = input('hola cual es tu nombre. ')
+print('Hola como estas  ' + name)
+
+
+after = input()
+print('a ok enserio estas ' + after)
+
+lol = input('')
+print(' a ok yo estoy bien ' + name)
+
+aa = input('')
+print(' a ok disculpa no te escuche me debo de ir, adios ' + name)
+
+ee = input('')
+print('')
